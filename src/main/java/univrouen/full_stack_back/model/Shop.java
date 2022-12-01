b3a776b32c7f9a08f4821c309d63aef3cdd9722a
safae jerradi <jerradisafae12@gmail.com>
2022-12-01 10:48:09 +0100

    feat: update shop
    add update shop controller, service
    add swagger doc for update shop

--- src/main/java/univrouen/full_stack_back/model/Shop.java
+++ src/main/java/univrouen/full_stack_back/model/Shop.java
@@ -3,21 +3,22 @@ package univrouen.full_stack_back.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import java.util.ArrayList;
-import java.util.Date;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalTime;
+import java.util.*;
 
 @Table(name="Shop")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Entity
 public class Shop {
     @Id
@@ -31,14 +32,13 @@ public class Shop {
     private  String name;
 
     @Column(name="closed")
-    @NotBlank(message = "closed is mandatory")
+    @NotNull(message = "closed is mandatory")
     @Schema(example = "false")
     private  Boolean closed;
-    @Column(name="opening_time")
-    @NotBlank(message = "opening_time is mandatory")
-    private ArrayList<Date> opening_time = new ArrayList(6);
-    @Column(name="closing_time")
-    @NotBlank(message = "closing_time is mandatory")
-    private ArrayList<Date>  closing_time = new ArrayList(6);
 
+    @Column(name="schedule")
+    @NotEmpty(message = "schedule is mandatory")
+    @Size(min=1, max=7)
+    @Schema(example = "\"lundi\": [\"9-15\"], \"mardi\": [\"6-12\", \"15-18\"]")
+    private HashMap<String, List<String>> schedule;
 }
