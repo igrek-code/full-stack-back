aea869f12f804a3cdf051082c2827b9f7e44e65a
safae jerradi <jerradisafae12@gmail.com>
2022-12-01 13:18:21 +0100

    feat: delete shop
    add delete shop controller, service
    add swagger doc for delete shop

--- src/main/java/univrouen/full_stack_back/service/ShopService.java
+++ src/main/java/univrouen/full_stack_back/service/ShopService.java
@@ -1,5 +1,6 @@
 package univrouen.full_stack_back.service;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import univrouen.full_stack_back.model.Shop;
 
 import java.util.List;
@@ -9,4 +10,5 @@ public interface ShopService {
     Shop save(Shop shop);
     Optional<Shop> findById(long id);
     Shop update(Long shopId,Shop shop);
+    void delete(Long id);
 }
